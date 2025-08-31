85e4801270b01762d971de945bc0478855695520
Tina <yutingtchuang@gmail.com>
2025-09-01 01:17:03 +0800

    feat: 實作 TodoController 與 WebApi 相關設定

---
+++ TodoApp.Api/Controllers/TodoController.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Mvc;
+using TodoApp.Application.Services;
+using TodoApp.Domain.Entities;
+
+namespace TodoApp.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TodoController : ControllerBase
+{
+    private readonly TodoService _service;
+
+    public TodoController(TodoService service)
+    {
+        _service = service;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll() => Ok(await _service.GetAllAsync());
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> Get(int id)
+    {
+        var todo = await _service.GetByIdAsync(id);
+        return todo == null ? NotFound() : Ok(todo);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] Todo todo)
+    {
+        var created = await _service.AddAsync(todo);
+        return CreatedAtAction(nameof(Get), new { id = created.Id }, created);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Update(int id, [FromBody] Todo todo)
+    {
+        if (id != todo.Id) return BadRequest();
+        await _service.UpdateAsync(todo);
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        await _service.DeleteAsync(id);
+        return NoContent();
+    }
+}
